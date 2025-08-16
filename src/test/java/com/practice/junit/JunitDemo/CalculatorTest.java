f2598191a72f9b41a2a531150de1ee60884fcfaf
Aditya Marchaya <aditya.marchaya@gmail.com>
2025-08-16 16:27:40 -0400

    First Commit of JUnit with JUnit 5

---
+++ src/test/java/com/practice/junit/JunitDemo/CalculatorTest.java
@@ -0,0 +1,14 @@
+package com.practice.junit.JunitDemo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    void testAddition() {
+        Calculator calc = new Calculator();
+        int result = calc.add(2, 3);
+        assertEquals(5, result, "Addition should be correct");
+    }
+}
